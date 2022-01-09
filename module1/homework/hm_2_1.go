3c127a124450ed9cfc818220768ee812447c6c15
YoKenshin <350833698@qq.com>
2022-01-09 22:06:35 +0800

    Correction

--- module1/homework/hm_2_1.go
+++ module1/homework/hm_2_1.go
@@ -3,8 +3,10 @@ package main
 import (
 	"fmt"
 	"log"
+	"net"
 	"net/http"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -19,19 +21,45 @@ func healthz(res http.ResponseWriter, req *http.Request) {
 
 	//1.接收客户端 request，并将 request 中带的 header 写入 response header
 	for k, v := range req.Header {
-		fmt.Fprintln(res, k+":", v)
+		//fmt.Fprintln(res, k+":", v)
+		for _, res_v := range v {
+			fmt.Printf("Header key: %s, Header value: %s \n", k, v)
+			res.Header().Set(k, res_v)
+		}
 	}
 
 	//2.读取当前系统的环境变量中的 VERSION 配置，并写入 response header
+	os.Setenv("VERSION", "21.1.1")
 	version := os.Getenv("VERSION")
-	fmt.Fprintf(res, "VERSION = [%q]\n", version)
+	res.Header().Set("VERSION", version)
+	fmt.Printf("VERSION = [%q]\n", version)
 
 	//3.Server 端记录访问日志，包括客户端 IP，HTTP 返回码，输出到 server 端的标准输出
-	ClientIPAddr := req.RemoteAddr
+	ClientIPAddr := ClientIP(req)
 	HttpCode := 200
-	fmt.Printf("ClientIPAddr = %s\nStatus = %d\n", ClientIPAddr, HttpCode)
+	log.Printf("Success! clientip: %s", ClientIPAddr)
+	log.Printf("Success! Response code: %d", HttpCode)
 
 	//4.当访问 localhost/healthz 时，应返回 200
 	res.WriteHeader(200)
 
+}
+
+// ClientIP 尽最大努力实现获取客户端 IP 的算法。
+// 解析 X-Real-IP 和 X-Forwarded-For 以便于反向代理（nginx 或 haproxy）可以正常工作。
+func ClientIP(r *http.Request) string {
+	xForwardedFor := r.Header.Get("X-Forwarded-For")
+	ip := strings.TrimSpace(strings.Split(xForwardedFor, ",")[0])
+	if ip != "" {
+		return ip
+	}
+
+	ip = strings.TrimSpace(r.Header.Get("X-Real-Ip"))
+	if ip != "" {
+		return ip
+	}
+	if ip, _, err := net.SplitHostPort(strings.TrimSpace(r.RemoteAddr)); err == nil {
+		return ip
+	}
+	return ""
 }
